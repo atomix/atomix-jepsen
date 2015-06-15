1fb2ea66fc963a2c06be98d738c3c380bdc5e1f6
Jonathan Halterman <jhalterman@gmail.com>
2015-06-15 12:42:07 -0700

    Noop nemesis for now

--- src/copycat_jepsen/core.clj
+++ src/copycat_jepsen/core.clj
@@ -210,7 +210,7 @@
             :model   (model/cas-register)
             :checker   (checker/compose {:html timeline/html
                                          :linear checker/linearizable})
-            :nemesis  (nemesis/partition-random-halves)
+            :nemesis  (nemesis/noop)
             :ssh      {:private-key-path "/home/vagrant/.ssh/id_rsa"}
             :node-set node-set})))
 
