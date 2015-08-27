1135c3d16139559ebfb2b0193efab3d7c8b35435
Jonathan Halterman <jhalterman@gmail.com>
2015-08-26 17:23:47 -0700

    Simplify nested container image - let jepsen's initial install do the heavy lifting

--- docker/build-docker-jepsen.sh
+++ docker/build-docker-jepsen.sh
@@ -16,16 +16,7 @@ N1_IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' n1)
 sleep 10
 
 # Initial setup
-ssh $N1_IP "rm /etc/apt/apt.conf.d/docker-clean && apt-get install sudo"
-
-# Install JDK 8
-ssh $N1_IP "echo 'deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main' | tee /etc/apt/sources.list.d/webupd8team-java.list"
-ssh $N1_IP "echo 'deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main' | tee -a /etc/apt/sources.list.d/webupd8team-java.list"
-ssh $N1_IP "echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections"
-ssh $N1_IP "apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886 && apt-get update -y -q && apt-get install -qqy oracle-java8-installer oracle-java8-set-default"
-
-# Install other dependencies and remove systemd
-ssh $N1_IP "apt-get install -qqy git maven net-tools wget sysvinit-core sysvinit sysvinit-utils curl vim man faketime unzip iptables iputils-ping logrotate && apt-get remove -y --purge --auto-remove systemd" 
+ssh $N1_IP "rm /etc/apt/apt.conf.d/docker-clean && apt-get update && apt-get install sudo net-tools wget sysvinit-core sysvinit sysvinit-utils curl vim man faketime unzip iptables iputils-ping logrotate && apt-get remove -y --purge --auto-remove systemd" 
 
 # Export the sub-container
 docker export n1 > /root/jepsennode.tar
